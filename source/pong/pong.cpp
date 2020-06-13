5fc643e36909326b2bc0615ce290a4524d47ebcb
DaDamola <58088213+DaDamola@users.noreply.github.com>
2020-06-13 14:30:25 -0400

    Create pong.cpp

---
+++ source/pong/pong.cpp
@@ -0,0 +1,24 @@
+#include "util.hpp"
+#include "ObjectManager.hpp"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <3ds.h>
+#include <m3dia.hpp>
+
+using namespace m3d;
+
+extern "C"
+{
+    #include "lua/lua.h"
+    #include "lua/lualib.h"
+    #include "lua/lauxlib.h"
+}
+
+int main(int argc, char* argv[])
+{
+
+	
+	return 0;
+}
